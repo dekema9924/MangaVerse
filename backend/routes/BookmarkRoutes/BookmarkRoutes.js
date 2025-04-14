8c45a7de31274fa692699792f5d6d96fbe54bc07
Daniel <daniel@Daniels-MBP.attlocal.net>
2025-04-14 16:09:41 -0400

    bookmark err

--- backend/routes/BookmarkRoutes/BookmarkRoutes.js
+++ backend/routes/BookmarkRoutes/BookmarkRoutes.js
@@ -4,13 +4,13 @@ const express = require('express')
 const verifyToken = require('../../middleware/verifyToken')
 const bookmarkRouter = express.Router()
 const addbookmark = require('../../controllers/BookmarkController/addbookmark')
-const getbookmark = require('../../controllers/BookmarkController/getbookmarks')
-const deletebookMark = require('../../controllers/BookmarkController/deletebookmark')
+const getbookmark = require('../../controllers/BookmarkController/getbookmark')
+const deletebookmark = require('../../controllers/BookmarkController/deletebookmark')
 
 
 bookmarkRouter.post('/addbookmarks', verifyToken, addbookmark)
 bookmarkRouter.get('/bookmarks', verifyToken, getbookmark)
-bookmarkRouter.delete('/bookmarks/:id', verifyToken, deletebookMark)
+bookmarkRouter.delete('/bookmarks/:id', verifyToken, deletebookmark)
 
 
 
