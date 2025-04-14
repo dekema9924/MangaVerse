61f3303ed6cd0c468e61885ed3ef9b71b208b643
Daniel <daniel@Daniels-MBP.attlocal.net>
2025-04-14 16:02:10 -0400

    bookmark ctller

--- backend/routes/BookmarkRoutes/BookmarkRoutes.js
+++ backend/routes/BookmarkRoutes/BookmarkRoutes.js
@@ -4,13 +4,13 @@ const express = require('express')
 const verifyToken = require('../../middleware/verifyToken')
 const bookmarkRouter = express.Router()
 const addbookmark = require('../../controllers/BookmarkController/addbookmark')
-const getbookmarks = require('../../controllers/BookmarkController/getbookmark')
-const deleteBookMark = require('../../controllers/BookmarkController/deletebookmark')
+const getbookmarks = require('../../controllers/BookmarkController/getBookmarks')
+const deletebookMark = require('../../controllers/BookmarkController/deletebookmark')
 
 
 bookmarkRouter.post('/addbookmarks', verifyToken, addbookmark)
 bookmarkRouter.get('/bookmarks', verifyToken, getbookmarks)
-bookmarkRouter.delete('/bookmarks/:id', verifyToken, deleteBookMark)
+bookmarkRouter.delete('/bookmarks/:id', verifyToken, deletebookMark)
 
 
 
