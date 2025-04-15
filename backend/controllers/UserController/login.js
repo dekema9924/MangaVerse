20eeca70f41362f35373a8b0cf6c50ba43f9eb90
Daniel <daniel@Daniels-MBP.attlocal.net>
2025-04-15 19:34:58 -0400

    cookie err

--- backend/controllers/UserController/login.js
+++ backend/controllers/UserController/login.js
@@ -24,10 +24,14 @@ const login = (req, res) => {
                     //pass token in cookie
                     res.cookie('token', token, {
                         httpOnly: true,
-                        sameSite: process.env.NODE_ENV == 'production' ? "None" : 'Lax',
-                        secure: process.env.NODE_ENV == 'production' ? true : false,
+                        sameSite: 'None',
+                        //  process.env.NODE_ENV == 'production' ? "None" : 'Lax',
+                        secure: true,
+                        //  process.env.NODE_ENV == 'production' ? true : false,
                         maxAge: 1 * 24 * 60 * 60 * 1000 // 1 days
-                    }).status(200).json({ message: 'Login successful', user: { username: user.username, email: user.email, id: user._id } })
+                    })
+                    console.log('Cookie set on login response'); // Debug
+                    res.status(200).json({ message: 'Login successful', user: { username: user.username, email: user.email, id: user._id } })
 
 
 
