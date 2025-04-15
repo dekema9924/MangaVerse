61f34e8c8025f4f553bb10045275da7aaf33479d
Daniel <daniel@Daniels-MBP.attlocal.net>
2025-04-15 18:52:40 -0400

    cookies not sent on mobile

--- backend/controllers/UserController/userProfile.js
+++ backend/controllers/UserController/userProfile.js
@@ -1,13 +1,14 @@
 const userdb = require('../../models/userModel')
 
 
-const profile =(req, res)=>{
+const profile = (req, res) => {
 
     userdb.findById(req.user.id).select("-password")
-    .then((user)=>{
-        if(!user) return(res.status(400).json({message: 'Invalid userId'}))
+        .then((user) => {
+            if (!user) return (res.status(400).json({ message: 'Invalid userId' }))
+            console.log('Cookies received:', req.cookies); // Log incoming cookies
             return res.status(200).json(user)
-    })
+        })
 }
 
 module.exports = profile
